eca3d4bcc48f1b41b34362b4564f9f630dd135fc
Sameishly <Sameishly@hotmail.com>
2020-04-12 19:25:58 -0400

    Added css styles

--- hangman.js
+++ hangman.js
@@ -1,6 +1,5 @@
 // Modal box pops up
 const modalBtn = document.getElementById('startBtn');
-
 modalBtn.addEventListener('click', function () {
 	modal.style.display = 'block';
 });
@@ -16,40 +15,24 @@ const enterBtn = document.querySelector('.enter');
 enterBtn.addEventListener('click', userInput, false);
 
 function userInput() {
-	const userInput = document.getElementById('randomWords').value;
+	const input = document.getElementById('randomWords').value; // get the value of user input.
+	const splitWord = input.split(''); // split user input string into characters. Stored in separate variable.
+	// console.log(splitWord);
 
-	const result = document.getElementById('word');
+	const divResult = document.getElementById('word'); // storing result inside the div.
+	divResult.innerHTML = splitWord;
 
-	if (randomWords.length < 3) {
-		result.textContent = 'Word must have at least 3 characters';
-	} else {
-		result.innerHTML = randomWords;
+	const wordSlot = [];
+
+	for (let i = 0; i < input.length; i++) {
+		const createDiv = document.createElement('DIV');
+		const getDiv = document.getElementById('word');
+		const addSlots = document.createTextNode(wordSlot);
+		getDiv.appendChild(addSlots);
+		wordSlot[i] = '_';
+		console.log(wordSlot[i]);
 	}
+
+	// const unguessedLetters = input.length;
+	// console.log(unguessedLetters);
 }
-
-const hangmanUrl = 'http____';
-
-// function submit() {
-// 	const enterBtn = document.querySelector('.enter');
-// }
-
-// create an array of blank spaces. Fill the spaces with the clicked letter if present.
-
-// for (let i = 0; i < words.length; i++) {
-// 	answer[i] = '_';
-// }
-
-// create a callback function to pass in an array of the random words.
-
-// a Loop needs to be created. The variable has to decrease by 1 with each correct guess until it reaches 0, meaning the player has one.
-
-// function words(callback) {
-// 	callback(['bicycle', 'staff', 'tiger']);
-// // }
-
-// const Btns = document.getElementById('alphabetButtons');
-// // console.log(alphabetBtns);
-// Btns.addEventListener('click', function (e) {
-// 	// console.log(e.target);
-// 	// console.log(e);
-// });
