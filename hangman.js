b83ffde1263239c8a1c3853601a57bde361a6570
Sameishly <Sameishly@hotmail.com>
2020-04-15 12:39:16 -0400

    if else statement that reveals correct letter

--- hangman.js
+++ hangman.js
@@ -27,6 +27,7 @@ function userInput() {
 		// span contains each letter that player 2 will try to guess. Is appended to div class id
 		const span = document.createElement('span');
 		span.setAttribute('class', 'letter');
+		span.setAttribute('data-letter', character[i]);
 		span.innerText = character[i];
 		divResult.appendChild(span);
 	}
@@ -48,15 +49,22 @@ const alphabetButtons = document.querySelector('#alphabetButtons');
 
 // below is the event listener for the alphabet button.
 alphabetButtons.addEventListener('click', (event) => {
+	const letters = document.querySelectorAll('.letter');
 	let keyInput = event.target.innerHTML; //Targeting the HTML inside each alphabet button.
 	for (let i = 0; i < userGuess.length; i++) {
+		console.log(letters);
 		if (keyInput.toLowerCase() === userGuess[i]) {
+			for (let i = 0; i < letters.length; i++) {
+				// compare userGuess i to letters i . toggle class set to visible
+				if (keyInput.toLowerCase() === letters[i].dataset.letter) {
+					letters[i].classList.add('letter-reveal');
+				}
+			}
 			// document.querySelectorAll('letter').style.visiblity = 'visible';
-
-			alert('we have a match!');
-
+			// alert('we have a match!');
 			//replace the slot with the correct keyInput(letter chosen).
 			// alert(keyInput);
+			console.log(keyInput, userGuess);
 		}
 	}
 });
